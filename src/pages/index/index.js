174f11d27a94f959b92087a97808ee0062ebc965
江文强 <782108446@qq.com>
2017-04-05 18:56:01 +0800

    搭建首页顶部

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -8,7 +8,24 @@ Page({
    */
   data: {
     title: 'Index page',
-    userInfo: {}
+    userInfo: {},
+    navList: [{
+      navTitle: '排队取号',
+      navIcon: 'iconfont icon-shalou'
+    },
+    {
+      navTitle: '排队取号',
+      navIcon: 'iconfont icon-shalou'
+    },
+    {
+      navTitle: '排队取号',
+      navIcon: 'iconfont icon-shalou'
+    }],
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ]
   },
   /**
    * 生命周期函数--监听页面加载
